ad96aec2f06c1b4be13eba8a7997bbfdf0e0e96d
Mathias Meyer <meyer@paperplanes.de>
2014-05-26 15:56:31 +0200

    Use process.env as object.

--- statsd-config.js
+++ statsd-config.js
@@ -1,8 +1,8 @@
 {
   backends: ["statsd-librato-backend"],
   librato: {
-    email:  process.env("LIBRATO_USER"),
-    token:  process.env("LIBRATO_TOKEN"),
-    source: 'statsd-sinatra-' + process.env("DYNO")
+    email:  process.env["LIBRATO_USER"],
+    token:  process.env["LIBRATO_TOKEN"],
+    source: 'statsd-sinatra-' + process.env["DYNO"]
   }
 }
