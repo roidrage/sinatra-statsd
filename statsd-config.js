7bb9573bd1458e1a0681cccd5a7dbcd051a9f85b
Mathias Meyer <meyer@paperplanes.de>
2014-05-26 15:52:18 +0200

    Enable Librato backend.

--- statsd-config.js
+++ statsd-config.js
@@ -1,3 +1,8 @@
 {
-  dumpMessages: true
+  backends: ["statsd-librato-backend"],
+  librato: {
+    email:  process.env("LIBRATO_USER"),
+    token:  process.env("LIBRATO_TOKEN"),
+    source: 'statsd-sinatra-' + process.env("DYNO")
+  }
 }
